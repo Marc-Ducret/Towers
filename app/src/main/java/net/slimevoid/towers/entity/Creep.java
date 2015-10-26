38e224345e8786e4517010c2d0b452da00d9073a
VengeurK <vengeurk@gmail.com>
2015-10-26 23:01:18 +0100

    Creep sprite

--- app/src/main/java/net/slimevoid/towers/entity/Creep.java
+++ app/src/main/java/net/slimevoid/towers/entity/Creep.java
@@ -1,4 +1,18 @@
 package net.slimevoid.towers.entity;
 
+import net.slimevoid.math.Vec2;
+import net.slimevoid.towers.R;
+
 public class Creep extends Entity {
+
+
+    @Override
+    protected int getSpriteID() {
+        return R.drawable.testrobot;
+    }
+
+    @Override
+    protected Vec2 getSpriteSize() {
+        return Vec2.NULL.add(.5,  .5);
+    }
 }
